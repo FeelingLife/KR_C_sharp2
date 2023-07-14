47f3cb688c1336c18b0b3459126e5731449042fb
Nikita <crazefire@mail.ru>
2023-07-14 21:49:18 +0300

    Вводим 1 функцию

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿string[] array = {"Hello", "ok", "lipton", "row", "Kirill", "coli", "100", "fillin", ";"};
 int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3) count = count + 1;
+}
