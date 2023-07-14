34247c0c742ffdf67bd84766e853c60f70ff6c20
Nikita <crazefire@mail.ru>
2023-07-14 21:49:36 +0300

    Вводим 2 функцию

--- Program.cs
+++ Program.cs
@@ -5,3 +5,14 @@ for (int i = 0; i < array.Length; i++)
 {
     if (array[i].Length <= 3) count = count + 1;
 }
+
+string[] resArray = new string[count];
+int resArrayIndex = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        resArray[resArrayIndex] = array[i];
+        resArrayIndex = resArrayIndex + 1;
+    }
+}
