fe59a8e70927ed937e3679c335dc16c47ebf95b6
Nikita <crazefire@mail.ru>
2023-07-15 21:21:08 +0300

    complete(3)

--- KR.cs
+++ KR.cs
@@ -1,19 +1,19 @@
-﻿// string[] array = {"Hello", "ok", "lipton", "row", "Kirill", "coli", "100", "fillin", ";"};
-// int count = 0;
-// for (int i = 0; i < array.Length; i++)
-// {
-//     if (array[i].Length <= 3) count = count + 1;
-// }
+﻿string[] array = {"Hello", "ok", "lipton", "row", "Kirill", "coli", "100", "fillin", ";"};
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3) count = count + 1;
+}
 
-// string[] resArray = new string[count];
-// int resArrayIndex = 0;
-// for (int i = 0; i < array.Length; i++)
-// {
-//     if (array[i].Length <= 3)
-//     {
-//         resArray[resArrayIndex] = array[i];
-//         resArrayIndex = resArrayIndex + 1;
-//     }
-// }
+string[] resArray = new string[count];
+int resArrayIndex = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        resArray[resArrayIndex] = array[i];
+        resArrayIndex = resArrayIndex + 1;
+    }
+}
 
-// Console.WriteLine("Массив из строк, длина которых меньше, либо равна 3 символам: \n" + $"[{String.Join(", ", resArray)}]\n");
+Console.WriteLine("Массив из строк, длина которых меньше, либо равна 3 символам: \n" + $"[{String.Join(", ", resArray)}]\n");
